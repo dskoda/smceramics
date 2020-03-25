53cbcafdcbda18767e92c55853d55e90e70efbd5
Daniel <dskoda@mit.edu>
2020-03-25 17:57:10 -0400

    create wrapper for the model

--- cofactor/model.py
+++ cofactor/model.py
@@ -1,21 +1,35 @@
+import pickle
 import pandas as pd
 import numpy as np
 from scipy import stats
-import matplotlib.pyplot as plt
-import itertools
-import tqdm
 
-from sklearn.linear_model import LinearRegression, BayesianRidge
+from sklearn.linear_model import BayesianRidge
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
-def get_stats(y_true, y_pred):
-    rmse = np.sqrt(mean_squared_error(y_true, y_pred))
-    rsq, _ = stats.pearsonr(y_true, y_pred)
-    
-    return rmse, rsq
 
-reg  = BayesianRidge(compute_score=True)
+def LatticePredictor:
+    def __init__(self, regressor):
+        self._regressor = regressor
 
+    def save(self, filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(self._regressor, f)
 
+    @classmethod
+    def from_file(cls, filename):
+        with open(filename, 'rb') as f:
+            regressor = pickle.load(self, f)
+
+        return cls(regressor)
+
+    def predict(self, X):
+        return self._regressor.predict(X)
+
+    def get_stats(self, X, y):
+        y_pred = self.predict(X)
+        rmse = np.sqrt(mean_squared_error(y, y_pred))
+        rsq, _ = stats.pearsonr(y, y_pred)
+        
+        return rmse, rsq
 
